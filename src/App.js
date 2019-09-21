72091902341c62ca9919858792439a565414988e
Jordan <jordandoan@hotmail.com>
2019-09-20 19:31:18 -0700

    packages added necessary for app, set up folder layout for components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ExampleComponent from './components/ExampleComponent';
+import './App.scss';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      Welcome To Trip Split
+      <ExampleComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExampleComponent/ExampleComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './ExampleComponent.module.scss';
+
+const ExampleComponent = () => {
+  return (
+    <div className={styles.app}>
+      This is an example component!
+    </div>
+  )
+}
+
+export default ExampleComponent; 
