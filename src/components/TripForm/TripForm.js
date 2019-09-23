d4bdaec5e8c6b38228c1a9bb90cc32a232cf2573
Jordan <jordandoan@hotmail.com>
2019-09-23 11:39:20 -0700

    Able to validate trip name with ant design component

--- src/components/TripForm/TripForm.js
+++ src/components/TripForm/TripForm.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Icon, Button} from 'antd';
 
 import "./TripForm.scss";
 
-
-
 const TripForm = (props) => {
   let id = props.match.params.id;
   let status = (id ? "Edit" : "Add");
-  const { getFieldDecorator } = props.form;
+  let [trip, setTrip] = useState({name:""});
+  let [validation, setValidation] = useState({name: {type: 'success', help: null}})
+  
+  const handleChange = (event) => {
+    setTrip({name: event.target.value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  const validateField = (event) => {
+    let val = trip[event.target.name];
+    if (val.trim()) {
+      setValidation({...validation, [event.target.name]: {type: 'success', help: null}})
+    } else {
+      setValidation({...validation, [event.target.name]: {type: 'error', help: 'Required'}})
+    }
+  }
+
   return (
     <div>
       This is the {status} trip form!
-      <Form>
-        <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Username"
-              />,
-            )}
-
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form.Item
+          label="Destination:"
+          validateStatus={validation.name.type}
+          help={validation.name.help}
+          name="name"
+        >
+          <Input
+            prefix={<Icon type="paper-clip" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="Trip Name"
+            name="name"
+            onChange={(e) => handleChange(e)}
+            onBlur={(e) => validateField(e)}
+            value={trip.name}
+          />
         </Form.Item>
         <Button type="primary" htmlType="submit">
-          Add
+          Add Trip
         </Button>
       </Form>
     </div>
   )
 }
 
-export default Form.create()(TripForm);
+export default TripForm;
