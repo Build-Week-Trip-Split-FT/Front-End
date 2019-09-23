70fc829110db4e6e38652fcf6cb339f312a5c5c1
Anthony Hill <hillan1152@gmail.com>
2019-09-23 17:11:32 -0400

    Completed Login Using Formik

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,50 +1,57 @@
-import React, { useState } from "react";
-import { withFormik, Field } from "formik";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
 import axios from "axios";
-import * as yup from "yup";
+import * as Yup from "yup";
 // import styles from './ExampleComponent.scss';
 import "./Login.scss";
 
 
-function Login() {
+function Login( { isSubmitting, errors, touched, status }) {
     const [user, setUser] = useState({ username: '', password: ''});
+
+    useEffect(() => {
+        if(status) {
+            setUser([...user, status]);
+        }
+    }, [status])
 
     const handleChange = e => {
         setUser({ ...user, [e.target.name]: e.target.value })
     }
+    
+    //USED W/ REACT, CHANGED FOR FORMIK
     const handleSubmit = e => {
         e.preventDefault();
         setUser({ username: '', password: ''})
     }
-
-    
     return (
         <div className="login-page">
-            {console.log(user)}
             <h2>Login</h2>
-            <form onSubmit={ e => handleSubmit(e)}>
-                <label>
+            <Form>
+                <div>
                     Username: 
-                    <input 
+                    <Field 
                         type="text" 
                         name="username"
                         placeholder="User Name"
                         onChange={e => handleChange(e)}
                         value={user.username}
                         />
-                </label>
-                <label>
+                    {touched.username && errors.username && <p>{errors.username}</p>}
+                </div>
+                <div>
                     Password:
-                    <input 
-                        type="text" 
+                    <Field 
+                        type="password" 
                         name="password"
                         placeholder="Password"
                         onChange={e => handleChange(e)}
                         value={user.password}
                         />
-                </label>
-                <button>Submit</button>
-            </form>
+                    {touched.password && errors.password && <p>{errors.password}</p>}
+                </div>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
+            </Form>
         </div>
     )
 }
@@ -53,18 +60,36 @@ const LoginWithFormik = withFormik({
     mapPropsToValues: ({ username, password}) => {
         return {
             username: username || '',
-            password: password || '',
+            password: password || ''
         };
     },
     //Validation
-    validationSchema: yup.object().shape({
-        username: yup.string().required,
-        password: yup.string().required
+    validationSchema: Yup.object().shape({
+        username: Yup.string("Username is not valid").required("Username is required"),
+        password: Yup.string().required("Password is required")
     }),
 
-    handleSubmit(values) {
-        console.log(values);
+    handleSubmit(values, { resetForm, setSubmitting }) {
+        const dataConfirm = { date: "Data Confirmed"}
+        axios
+        .post("https://reqres.in/api/users", dataConfirm)
+        .then(res => {
+            console.log(res.data)
+            resetForm();
+            setSubmitting(false)
+        })
+        .catch(error => {
+            console.log(error)
+            
+        })
     }
 })(Login);
+
+
+
+
+
+
+
 
 export default LoginWithFormik;
