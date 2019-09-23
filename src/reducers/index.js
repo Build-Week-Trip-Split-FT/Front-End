de622c8f0c34101a209d0d9d696957aa6c03ee1b
Jordan <jordandoan@hotmail.com>
2019-09-23 14:12:15 -0700

    Added more reducers, changed mockData to match the actual data shape more

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,14 @@ import { mockData } from '../utils';
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
     case FETCHING:
-      return {isFetching: true, title: "I am fetching!"}
+      return {...state, isFetching: true, fetching_message: "I am fetching!"}
+    case LOGGING_IN:
+      return {...state, isFetching: true, fetching_message: "Logging in User..."}
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload);
+      return {...state, isFetching: false, fetching_message: "", error: false}
+    case LOGIN_FAILURE:
+      return {...state, isFetching: false, fetching_message: "", error: true, error_message: action.payload}
     case TOGGLE_PAID: {
       return {...state, currentData: {...state.currentData, people: action.payload}}
     }
@@ -17,4 +24,6 @@ const initialState = {
   currentData: mockData,
   isFetching: false,
   fetching_message: "",
+  error: false,
+  error_message: "",
 };
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,13 +1,69 @@
-export const mockData = {
-    name: "Example Trip",
-    people: [
-        {
-             name: "Jordan",
-            paid: false,
-        },
-        {
-            name: "Jeffrey",
-            paid: true,
-        }
-    ],
-}
+export const mockData = [
+    {
+        destination: "Bahamas",
+        date: "09/29/19",
+        active: true,
+        created_by: "Jordan",
+        people: [
+          {
+            first_name: "Jordan",
+            last_name: "Doan",
+          },
+          {
+            first_name: "Bill",
+            last_name: "Frank"
+          }
+        ],
+        expenses: [
+          {
+            name:"Dinner",
+            amount:50,
+            person_name:"Bill",
+            debts: [
+                {
+                    person_name: "Jordan",
+                    amount: "25",
+                },
+                {
+                    person_name: "Bill",
+                    amount:"25",
+                },
+            ]
+          }
+        ]
+    },
+    {
+        destination: "Bahamas",
+        date: "09/29/19",
+        active: true,
+        created_by: "Jordan",
+        people: [
+          {
+            first_name: "Jordan",
+            last_name: "Doan",
+          },
+          {
+            first_name: "Bill",
+            last_name: "Frank"
+          }
+        ],
+        expenses: [
+          {
+            name:"Dinner",
+            amount:50,
+            person_name:"Bill",
+            debts: [
+                {
+                    person_name: "Jordan",
+                    amount: "25",
+                },
+                {
+                    person_name: "Bill",
+                    amount:"25",
+                },
+            ]
+          }
+        ]
+    },
+]
+
