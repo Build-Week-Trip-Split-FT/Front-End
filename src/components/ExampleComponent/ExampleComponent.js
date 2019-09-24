87077914852b2ac88cf978fccb5bc309f95f1529
Jordan <jordandoan@hotmail.com>
2019-09-23 22:44:41 -0700

    Added feature to toggle paid/unpaid status for a person for an expense

--- src/components/ExampleComponent/ExampleComponent.js
+++ src/components/ExampleComponent/ExampleComponent.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { togglePaid, setEvent } from '../../actions';
 
-import { togglePaid } from '../../actions';
-
-const ExampleComponent = ({ currentData, togglePaid }) => {
-  let trip = currentData[0];
-  console.log(trip);
+const ExampleComponent = ({ currentData, singleTrip, togglePaid, setEvent }) => {
   return (
     <div>
-      <p>Destination: {trip.destination}</p>
+      <p>Destination: {singleTrip.destination}</p>
       <p>Expenses</p>
-      {trip.expenses.map(expense => 
+
+      {singleTrip.expenses &&  
+      <div>
+        {singleTrip.expenses.map(expense => 
         <div>
           <h1>{expense.name}</h1>
           <h2>Paid</h2>
           {expense.debts.filter(person => person.amount === 0).map(person => 
             <div>
               {person.person_name}
-              <button onClick={togglePaid}>x</button>
+              <button onClick={() => togglePaid(person,expense,singleTrip)}>x</button>
             </div>
             )}
           <h2>Unpaid</h2>
           {expense.debts.filter(person => person.amount !== 0).map(person => 
             <div>
               {person.person_name}
-              <button>x</button>
+              <button onClick={() => togglePaid(person,expense,singleTrip)}>x</button>
             </div>
-            )}
+          )}
         </div>
-      )}
+        )}
+      </div>}
       {/* This is an example component!
       <h2>Event</h2>
-      <p>Name: {trip.name} </p>
+      <p>Name: {singleTrip.name} </p>
       <div>
         Paid
         {trip.people.filter(person => person.paid).map(person => <p>{person.name}<button onClick={() => togglePaid(person, trip.people)}>X</button></p>)}
@@ -45,6 +46,7 @@ const ExampleComponent = ({ currentData, togglePaid }) => {
 const mapStateToProps = state => {
   return {
     currentData: state.currentData,
+    singleTrip: state.singleTrip
   }
 };
 export default connect(mapStateToProps, {togglePaid: togglePaid})(ExampleComponent); 
