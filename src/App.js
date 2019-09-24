3ab5da56c2c8c758338b47e44e772e440bb924b3
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-09-24 13:32:28 -0700

    working on HomeView

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import ExampleComponent from "./components/ExampleComponent";
 import SignUp from "./components/SignUp/";
 import Login from "./components/Login/";
 import HomeView from "./components/HomeView/HomeView";
-import Message from './components/Message';
+import Message from "./components/Message";
 import NavBar from "./components/NavBar/NavBar";
 
-import './App.scss';
-
+import "./App.scss";
 
 const App = () => {
   const logOut = () => {
@@ -19,10 +18,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <NavBar/>
-     <div>
-        Welcome To Trip Split
-      </div>
+      <NavBar />
+      <div>Welcome To Trip Split</div>
       <ul>
         <li>
           <button onClick={logOut}>Log out</button>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,24 @@
 import React from "react";
-import "./NavBar.scss"
+import "./NavBar.scss";
 import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <div className="container-nav">
-        <h1>Trip Split</h1>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="">About</Link>
-            {/* <a href="#">Trips</a>
+  return (
+    <div className="container-nav">
+      <h1>Trip Split</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="">About</Link>
+        <Link to="/homeview">Home View</Link>
+        {/* <a href="#">Trips</a>
             <a href="#">About</a>
             <a href="#">Login</a>
             <a href="#">Sign Up</a> */}
-        </nav>
+      </nav>
     </div>
-    )
+  );
 }
 
 export default NavBar;
