c4e9b0028733e8cf60605c7cdd7d566662d5efb7
Jordan <jordandoan@hotmail.com>
2019-09-24 09:31:03 -0700

    removed react hooks from sign up form, used ant design's state managment

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import { Alert } from 'antd';
+import { connect } from 'react-redux';
+
+const Message = (props) => {
+  return (
+    <>
+      {props.error && <Alert message="Error" description={props.error_message} onClose/>}
+    </>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    error: state.error,
+    error_message: state.error_message,
+  }
+};
+export default connect(mapStateToProps,{})(Message);
