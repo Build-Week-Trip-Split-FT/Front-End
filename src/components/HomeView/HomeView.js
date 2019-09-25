a0dbd7a2fa397ced5495d7518933fd6f50554b0c
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-09-25 08:14:41 -0700

    Started working on google maps api

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import {GoogleMap} from 'react-google-maps';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 import { connect } from "react-redux";
 
 import { fetchUser } from "../../actions";
@@ -53,6 +53,15 @@ function UserDetails(props) {
           <p>Date: {props.date.toString()}</p>
           <p>Active: {props.active ? "Yes" : "No"}</p>
           <p>Number of People: {props.num_people}</p>
+          <div style={{ width: "20vw", height: "20rem" }}>
+            <WrappedMap
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBogHSf0D1ydBuNLDO0tYjZB_sN5r15Psw`}
+              loadingElement={<div style={{ height: "100%" }} />}
+              containerElement={<div style={{ height: "100%" }} />}
+              mapElement={<div style={{ height: "100%" }} />}
+              destination={props.destination}
+            />
+          </div>
         </div>
         <p></p>
       </div>
@@ -60,12 +69,17 @@ function UserDetails(props) {
   );
 }
 
-
-function Map() {
+function Map(props) {
   return (
-
-  )
+    <GoogleMap
+      defaultZoom={10}
+      // defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+      defaultCenter={props.destination}
+    />
+  );
 }
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const mapStateToProps = state => {
   return {
