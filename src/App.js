49d74a95eaef36c918390973b583d9bdc8e3ae61
Jordan <jordandoan@hotmail.com>
2019-09-26 11:48:13 -0700

    fixed folder structure for different components

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import { PrivateRoute } from "./utils";
 import ExampleComponent from "./components/ExampleComponent";
 import SignUp from "./components/SignUp/";
 import Login from "./components/Login/";
-import HomeView from "./components/HomeView/HomeView";
+import HomeView from "./components/HomeView/";
 import Fetching from './components/Fetching';
 import Message from "./components/Message";
-import NavBar from "./components/NavBar/NavBar";
-import AddTrip from "./components/TripForm/AddTrip";
+import NavBar from "./components/NavBar/";
+import AddTrip from "./components/TripForm/";
 import TripView from './components/TripView';
 import PeopleForm from './components/PeopleForm';
 import ExpenseForm from './components/ExpenseForm';
 import DebtForm from './components/DebtForm';
-import './App.scss';
 
+import './App.scss';
 
 const App = (props) => {
 
